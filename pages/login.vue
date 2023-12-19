3fdf7b00bf7cb5fa13b89ac788e7281051100dea
manuel.kramm <manuel.kramm@interone.de>
2023-12-19 08:59:18 +0100

    test

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-const props = defineProps({
-  image: {
-    type: String,
-    required: false,
-  },
-});
-
-const default_image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qCreqkTZL0F0bF9kZctFE1XVFocO__70kw&usqp=CAU";
 </script>
 
 <template>
   <div class="login-container">
     <div class="logo-container">
-<!--      <img :src="props.image ? props.image : default_image" alt="Logo"> &lt;!&ndash; Ersetzen Sie logo.png mit Ihrem Projekt-Logo &ndash;&gt;-->
     </div>
     <form class="login-form">
       <input type="text" placeholder="Benutzername" required>
